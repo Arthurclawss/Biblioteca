46acc0594a495faaa3b1494f6553ca5be79f8755
Arthurclawss <arthurclaws21@gmail.com>
2026-03-04 13:23:37 -0300

    Atualizando projeto completo com todas as classes

---
+++ src/main/java/med/voll/Api/Endereco/DadosEndereco.java
@@ -0,0 +1,4 @@
+package med.voll.Api.Endereco;
+
+public record DadosEndereco(String logradouro, String Bairro, String cep, String cidade, String uf, String complemento, String numero) {
+}
---
+++ src/main/java/med/voll/Api/Medico/Especialidade.java
@@ -0,0 +1,9 @@
+package med.voll.Api.Medico;
+
+public enum Especialidade {
+
+    ORTOPEDIA,
+    CARDIOLOGIA,
+    GINECOLOGIA,
+    DEMARTOLOGIA;
+}
